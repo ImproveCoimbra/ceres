8155589d57416d372cc5547216b17f5e11670ca6
Luís Cardoso <lfac.pt@gmail.com>
2014-06-14 16:55:23 +0100

    Adds an email to the fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -15,6 +15,7 @@ testPhotos = [
 fixturesArray = [
   {
     name: 'Dona Albertina',
+    email: "me@lfac.me",
     mainPhotoUrl : 'http://3.bp.blogspot.com/_jie4i34OAmg/S9nRZEG7E_I/AAAAAAAAAJA/D3jIVPlkIxU/s1600/Escola+Benfica+2010-1.JPG',
     description: 'Vendo couves a muito bom preço!!',
     photos: testPhotos,
@@ -22,6 +23,7 @@ fixturesArray = [
   },
   {
     name: 'Senhor Manuel',
+    email: "me@lfac.me",
     mainPhotoUrl: 'http://eventoclick.com/img/img_reportajes/2015_1313156948_prod%20bio.jpg',
     description: 'Sou especialista na produção de batata!',
     photos: testPhotos,
@@ -29,6 +31,7 @@ fixturesArray = [
   },
   {
     name: 'Irmãos Fonseca',
+    email: "me@lfac.me",
     mainPhotoUrl: 'http://t3.gstatic.com/images?q=tbn:ANd9GcRFU7EzjpFqy1HQuzGvV_2FShn2kLB2ggToVV_hVmLr0jCnARQr',
     description: 'Produzimos fruta de toda a espécie!!',
     photos: testPhotos,
@@ -36,6 +39,7 @@ fixturesArray = [
   },
   {
     name: 'Senhor Manuel 2',
+    email: "me@lfac.me",
     mainPhotoUrl: 'http://eventoclick.com/img/img_reportajes/2015_1313156948_prod%20bio.jpg',
     description: 'Sou especialista na produção de batata!',
     photos: testPhotos,
@@ -43,6 +47,7 @@ fixturesArray = [
   },
   {
     name: 'Irmãos Fonseca 2',
+    email: "me@lfac.me",
     mainPhotoUrl: 'http://t3.gstatic.com/images?q=tbn:ANd9GcRFU7EzjpFqy1HQuzGvV_2FShn2kLB2ggToVV_hVmLr0jCnARQr',
     description: 'Produzimos fruta de toda a espécie!!',
     photos: testPhotos,
